59932add02f03093c96cdca93565dafe62043177
Max Maeder <max.s.maeder@gmail.com>
2023-10-28 14:05:56 -0500

    Change logo, fix map

--- hhi-app/src/App.tsx
+++ hhi-app/src/App.tsx
@@ -13,14 +13,14 @@ export default function App() {
 
     <div className="relative h-full">
       <div className="absolute top-0 left-0 right-0 z-[1000] flex justify-end p-5 header-drop">
-        <img src="hhi-logo.png" alt="Harvard Logo" className="h-30"/>
+        <img src="att-logo.png" alt="Harvard Logo" className="h-20 filter drop-shadow invert"/>
       </div>
 
   <MapContainer className="w-full h-full" center={[51.505, -0.09]} zoom={zoomLevel} scrollWheelZoom={true} zoomControl={false}>
 
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+      url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${process.env.STADIA_KEY}`}
     />
 
     <div className="absolute left-4 bottom-5 z-[1000]">
