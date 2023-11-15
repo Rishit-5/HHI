4c510d8239d3340ea73754672c561764b6e4ca56
Elon <aidencohen31@gmail.com>
2023-11-14 20:00:40 -0600

    db changes

--- hhi-app/src/App.tsx
+++ hhi-app/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { useEffect } from "react";
 import Map from "components/Map/Map";
-
+import loadObjects from 'helper';
 import "leaflet/dist/leaflet.css";
 
 const App: React.FC = () => {
   const stadiaAPIKey = import.meta.env.VITE_STADIA_KEY;
+  useEffect(() => {
 
+    console.log(loadObjects());
+
+
+  }, [])
   return (
     <div className="relative h-full">
       <div className="absolute top-0 left-0 right-0 z-[1000] flex justify-end p-5 header-drop">
