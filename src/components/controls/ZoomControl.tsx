ada8d8beb31816a11e7ee531f19360eb2916567d
GeoffreyXue <xuegeoff@gmail.com>
2024-02-02 11:32:06 -0600

    Add custom control component, reorganize controls

--- src/components/controls/ZoomControl.tsx
+++ src/components/controls/ZoomControl.tsx
@@ -1,5 +1,5 @@
 import { useMap } from 'react-leaflet'
-import 'leaflet-search'
+import Control from 'react-leaflet-custom-control'
 
 interface ZoomControlProps {
   zoomInTitle?: string
@@ -14,41 +14,43 @@ function ZoomControl(props: ZoomControlProps) {
   const map = useMap()
 
   return (
-    <div className="leaflet-control leaflet-bar absolute bottom-5 right-4 z-[1000]">
-      <a
-        className="leaflet-control-zoom-in"
-        title={zoomInTitle}
-        role="button"
-        onClick={(e) => {
-          map.zoomIn()
-          e.preventDefault()
-        }}
-      >
-        +
-      </a>
-      <a
-        className="leaflet-control-zoom-out"
-        title={zoomOutTitle}
-        role="button"
-        onClick={(e) => {
-          map.zoomOut()
-          e.preventDefault()
-        }}
-      >
-        -
-      </a>
-      <a
-        className="leaflet-control-zoom-out"
-        title={zoomResetTitle}
-        role="button"
-        onClick={(e) => {
-          map.setZoom(zoomLevel)
-          e.preventDefault()
-        }} // circle arrow symbol
-      >
-        &#x21ba;
-      </a>
-    </div>
+    <Control position="topleft">
+      <div className="leaflet-bar">
+        <a
+          className="leaflet-control-zoom-in"
+          title={zoomInTitle}
+          role="button"
+          onClick={(e) => {
+            map.zoomIn()
+            e.preventDefault()
+          }}
+        >
+          +
+        </a>
+        <a
+          className="leaflet-control-zoom-out"
+          title={zoomOutTitle}
+          role="button"
+          onClick={(e) => {
+            map.zoomOut()
+            e.preventDefault()
+          }}
+        >
+          -
+        </a>
+        <a
+          className="leaflet-control-zoom-out"
+          title={zoomResetTitle}
+          role="button"
+          onClick={(e) => {
+            map.setZoom(zoomLevel)
+            e.preventDefault()
+          }} // circle arrow symbol
+        >
+          &#x21ba;
+        </a>
+      </div>
+    </Control>
   )
 }
 
