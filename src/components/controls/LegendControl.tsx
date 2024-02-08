fbc7d527e55d04f84646dc917ab71a5be07ee072
rpatil5 <rishistar05@gmail.com>
2024-02-08 17:14:31 -0600

    removed black dots and updated legend

--- src/components/controls/LegendControl.tsx
+++ src/components/controls/LegendControl.tsx
@@ -10,16 +10,12 @@ const LegendControl: React.FC<LegendControlProps> = ({ selectedStakeholder }) =>
             {selectedStakeholder && (
                 <div className="Legend-div" style={{ position: 'absolute', bottom: '10px', right: '10px', zIndex: 1000, backgroundColor: 'rgba(0,0,0,0.5)', padding: '10px', margin: '10px' }}>
                     <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px' }}>
-                        <img src={"public/selected-marker.svg"} alt="Marker 1" style={{ width: '30px', height: '30px', marginRight: '5px'}} />
-                        <span className="text-white">Headquarters</span>
+                        <img src={"public/marker.svg"} alt="Marker 1" style={{ width: '30px', height: '30px', marginRight: '5px'}} />
+                        <span className="text-white">Headquarters/Registration Location</span>
                     </div>
-
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '0px' }}>
-                        <svg width="30px" height="30px" style={{ marginRight: '5px' }}>
-                            {/* Your SVG content for the first legend item */}
-                            <circle cx="15" cy="15" r="12" fill="black" />
-                        </svg>
-                        <span className="text-white">Areas Served</span>
+                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px' }}>
+                        <img src={"public/selected-marker.svg"} alt="Marker 1" style={{ width: '30px', height: '30px', marginRight: '5px'}} />
+                        <span className="text-white">Selected</span>
                     </div>
                 </div>
             )}
