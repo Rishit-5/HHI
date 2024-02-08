5ad520abc1d5bbf9f0a443821878a01a2ffc4765
rpatil5 <rishistar05@gmail.com>
2024-02-08 17:22:28 -0600

    updated zoom constraints

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -14,11 +14,17 @@ interface MapProps {
 }
 
 const Map: React.FC<MapProps> = ({ apiKey, stakeholders }) => {
+    const maxBounds = [
+        // Southwest coordinate
+        [-90, -180],
+        // Northeast coordinate
+        [90, 180],
+    ];
   const [selectedStakeholder, setSelectedStakeholder] = useState<Stakeholder | null>(null)
   const markersLayer = useRef(null)
 
   return (
-    <MapContainer className="w-full h-full" center={[20, 0]} zoom={3} scrollWheelZoom={true} zoomControl={false}>
+    <MapContainer className="w-full h-full" center={[20, 0]} zoom={3} minZoom={2} scrollWheelZoom={true} zoomControl={false} maxBounds={maxBounds}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${apiKey}`}
