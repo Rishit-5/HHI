a1c2bf98bba7002eaae4141986caaa0787d52a05
rpatil5 <rishistar05@gmail.com>
2024-02-08 17:29:20 -0600

    updated zoom constraints and with black circles

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -24,7 +24,7 @@ const Map: React.FC<MapProps> = ({ apiKey, stakeholders }) => {
   const markersLayer = useRef(null)
 
   return (
-    <MapContainer className="w-full h-full" center={[20, 0]} zoom={3} minZoom={2} scrollWheelZoom={true} zoomControl={false} maxBounds={maxBounds}>
+    <MapContainer className="w-full h-full" center={[20, 0]} zoom={3} minZoom={3} scrollWheelZoom={true} zoomControl={false} maxBounds={maxBounds}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${apiKey}`}
