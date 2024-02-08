00c0f00c82f0941585ed283c489c87e4533622a5
GeoffreyXue <xuegeoff@gmail.com>
2024-02-08 17:15:16 -0600

    style -> tailwind styling, flip colors

--- src/components/controls/LegendControl.tsx
+++ src/components/controls/LegendControl.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from 'react'
 
 interface LegendControlProps {
-    selectedStakeholder: any;
+  selectedStakeholder: any
 }
 
 const LegendControl: React.FC<LegendControlProps> = ({ selectedStakeholder }) => {
-    return (
-        <>
-            {selectedStakeholder && (
-                <div className="Legend-div" style={{ position: 'absolute', bottom: '10px', right: '10px', zIndex: 1000, backgroundColor: 'rgba(0,0,0,0.5)', padding: '10px', margin: '10px' }}>
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '5px' }}>
-                        <img src={"public/selected-marker.svg"} alt="Marker 1" style={{ width: '30px', height: '30px', marginRight: '5px'}} />
-                        <span className="text-white">Headquarters</span>
-                    </div>
+  return (
+    <>
+      {selectedStakeholder && (
+        <div className="absolute bottom-3 right-3 z-[1000] m-3 rounded-md bg-white bg-opacity-50 p-3">
+          <div className="mt-1 flex items-center">
+            <img src={'selected-marker.svg'} alt="Marker 1" className="mr-1 h-8 w-8" />
+            <span className="font-metropolis text-black-">Headquarters</span>
+          </div>
 
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '0px' }}>
-                        <svg width="30px" height="30px" style={{ marginRight: '5px' }}>
-                            {/* Your SVG content for the first legend item */}
-                            <circle cx="15" cy="15" r="12" fill="black" />
-                        </svg>
-                        <span className="text-white">Areas Served</span>
-                    </div>
-                </div>
-            )}
-        </>
-    );
-};
+          <div className="mt-0 flex items-center">
+            <svg width="30px" height="30px" className="mr-1">
+              {/* Your SVG content for the first legend item */}
+              <circle cx="15" cy="15" r="12" fill="black" />
+            </svg>
+            <span className="font-metropolis text-black-">Areas Served</span>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
 
-export default LegendControl;
+export default LegendControl
